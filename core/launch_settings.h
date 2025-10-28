805d2d204fc2ad7725cebff7705d18fba80b361f
Martin Dufour <88708424+MartinDew@users.noreply.github.com>
2025-10-28 14:48:14 -0400

    Made the editor mode an implicit argument and launch settings fix (#4)
    Co-authored-by: md <martin.dufour@havenstudios.com>

--- core/launch_settings.h
+++ core/launch_settings.h
@@ -14,20 +14,20 @@ class LaunchSettings {
 public:
 	LaunchSettings(int argc, char* argv[]);
 
-	args::Positional<std::filesystem::path> project_path{ _parser, "project path", "the path to the project directory", std::filesystem::current_path().c_str() };
+	args::Positional<std::filesystem::path> project_path{ _parser, "project path", "The path to the project directory", std::filesystem::current_path().c_str() };
 
 #ifdef EDITOR_MODE
 	// Editor mode?
-	args::ValueFlag<bool> editor_mode{ _parser, "editor", "should run in editor mode", { 'e', "editor" }, true };
+	args::ImplicitValueFlag<bool> editor_mode{ _parser, "editor", "Should run in editor mode", { 'e', "editor" }, true, false };
 #endif
 
 	// will be more complex eventually but for now just a flag for windowed vs dummy
-	args::ValueFlag<std::string> windowed{ _parser, "window mode", "the window mode to use (windowed {default} | headless )", { "w" }, "windowed" };
+	args::ValueFlag<std::string> windowed{ _parser, "window mode", "The window mode to use (windowed {default} | headless )", { "w" }, "windowed" };
 
 	static LaunchSettings& get() { return *instance; }
 
 private:
-	args::HelpFlag _help{ _parser, "help", "display this help menu", { 'h', "help" } };
+	args::HelpFlag _help{ _parser, "help", "Display this help menu", { 'h', "help" } };
 };
 
 } //namespace feather
