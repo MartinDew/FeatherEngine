255ad8429726ef8ce431d2e6e24163b5c9463ee9
md <martin.dufour@havenstudios.com>
2025-11-03 12:48:24 -0500

    Fix use of format in math.h and added option to use llvm compilers.

--- core/math.h
+++ core/math.h
@@ -1,8 +1,9 @@
 #pragma once
+#include <format>
+#include <utility>
+
 #include <DirectXMath.h>
 #include <SimpleMath.h>
-
-#include <utility>
 
 namespace feather {
 
@@ -94,60 +95,60 @@ inline Vector3 get_origin(const Matrix& mat) {
 
 inline constexpr float SMALL_NUMBER = 1.e-4f;
 inline constexpr float QUATERNION_NORMALIZE_THRESHHOLD = 0.01f;
-//
-// template <>
-// struct std::formatter<Quaternion> {
-// 	constexpr auto parse(std::format_parse_context& ctx) {
-// 		return ctx.begin();
-// 	}
-//
-// 	auto format(const Quaternion& quat, std::format_context& ctx) const {
-// 		return std::format_to(ctx.out(), "X={}, Y={}, Z={}, W={}", quat.x, quat.y, quat.z, quat.w);
-// 	}
-// };
-//
-// template <>
-// struct std::formatter<Vector4> {
-// 	constexpr auto parse(std::format_parse_context& ctx) {
-// 		return ctx.begin();
-// 	}
-//
-// 	auto format(const Vector4& vec, std::format_context& ctx) const {
-// 		return std::format_to(ctx.out(), "X={}, Y={}, Z={}, W={}", vec.x, vec.y, vec.z, vec.w);
-// 	}
-// };
-//
-// template <>
-// struct std::formatter<Vector3> {
-// 	constexpr auto parse(std::format_parse_context& ctx) {
-// 		return ctx.begin();
-// 	}
-//
-// 	auto format(const Vector3& vec, std::format_context& ctx) const {
-// 		return std::format_to(ctx.out(), "X={}, Y={}, Z={}", vec.x, vec.y, vec.z);
-// 	}
-// };
-//
-// template <>
-// struct std::formatter<Vector2> {
-// 	constexpr auto parse(std::format_parse_context& ctx) {
-// 		return ctx.begin();
-// 	}
-//
-// 	auto format(const Vector2& vec, std::format_context& ctx) const {
-// 		return std::format_to(ctx.out(), "X={}, Y={}", vec.x, vec.y);
-// 	}
-// };
-//
-// template <>
-// struct std::formatter<Color> {
-// 	constexpr auto parse(std::format_parse_context& ctx) {
-// 		return ctx.begin();
-// 	}
-//
-// 	auto format(const Color& col, std::format_context& ctx) const {
-// 		return std::format_to(ctx.out(), "R={}, G={}, B={}, A={}", col.x, col.y, col.z, col.w);
-// 	}
-// };
 
 } //namespace feather
+
+template <>
+struct std::formatter<feather::Quaternion> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const feather::Quaternion& quat, std::format_context& ctx) const {
+		return std::format_to(ctx.out(), "X={}, Y={}, Z={}, W={}", quat.x, quat.y, quat.z, quat.w);
+	}
+};
+
+template <>
+struct std::formatter<feather::Vector4> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const feather::Vector4& vec, std::format_context& ctx) const {
+		return std::format_to(ctx.out(), "X={}, Y={}, Z={}, W={}", vec.x, vec.y, vec.z, vec.w);
+	}
+};
+
+template <>
+struct std::formatter<feather::Vector3> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const feather::Vector3& vec, std::format_context& ctx) const {
+		return std::format_to(ctx.out(), "X={}, Y={}, Z={}", vec.x, vec.y, vec.z);
+	}
+};
+
+template <>
+struct std::formatter<feather::Vector2> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const feather::Vector2& vec, std::format_context& ctx) const {
+		return std::format_to(ctx.out(), "X={}, Y={}", vec.x, vec.y);
+	}
+};
+
+template <>
+struct std::formatter<feather::Color> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const feather::Color& col, std::format_context& ctx) const {
+		return std::format_to(ctx.out(), "R={}, G={}, B={}, A={}", col.x, col.y, col.z, col.w);
+	}
+};
--- tools/options.cmake
+++ tools/options.cmake
@@ -1,1 +1,18 @@
 option(STATIC_CPP "STATIC_CPP" ON)
+
+
+if (WIN32)
+    option(USE_LLVM "USE_LLVM" ON)
+else()
+    option(USE_LLVM "USE_LLVM" OFF)
+endif()
+
+if (${USE_LLVM})
+    if (MSVC)
+        set(CMAKE_C_COMPILER "clang-cl" CACHE STRING "The Cmake C Compiler")
+        set(CMAKE_CXX_COMPILER "clang-cl" CACHE STRING "The Cmake CXX Compiler")
+    else()
+        set(CMAKE_C_COMPILER "clang" CACHE STRING "The Cmake C Compiler")
+        set(CMAKE_CXX_COMPILER "clang++" CACHE STRING "The Cmake CXX Compiler")
+    endif()
+endif()
