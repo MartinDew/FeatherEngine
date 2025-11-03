a274056db6087e141734bd91165b591d09f39a3c
Martin Dufour <sonic99@live.ca>
2025-11-03 18:27:23 -0500

    Modified some options

--- tools/options.cmake
+++ tools/options.cmake
@@ -1,25 +1,20 @@
 option(STATIC_CPP "STATIC_CPP" ON)
 
-
-if (WIN32)
-    option(USE_LLVM "USE_LLVM" ON)
-else()
-    option(USE_LLVM "USE_LLVM" OFF)
-endif()
+option(USE_LLVM "USE_LLVM" OFF)
 
 if (${USE_LLVM})
     if (WIN32 AND NOT MINGW)
         if (${CMAKE_GENERATOR} EQUAL "Visual Studio 17 2022")
             set(CMAKE_GENERATOR_TOOLSET "ClangCl")
-        else()
+        else ()
             set(CMAKE_C_COMPILER "clang-cl" CACHE STRING "The Cmake C Compiler")
             set(CMAKE_CXX_COMPILER "clang-cl" CACHE STRING "The Cmake CXX Compiler")
-        endif()
-    else()
+        endif ()
+    else ()
         set(CMAKE_C_COMPILER "clang" CACHE STRING "The Cmake C Compiler")
         set(CMAKE_CXX_COMPILER "clang++" CACHE STRING "The Cmake CXX Compiler")
-    endif()
-endif()
+    endif ()
+endif ()
 
 # Clang-Tidy integration
 option(ENABLE_CLANG_TIDY "Enable clang-tidy checks during compilation" OFF)
