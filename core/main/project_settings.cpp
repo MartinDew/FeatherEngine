bbd81a0201c41f5d65ddabaf6281ca39e0a5a071
md <martin.dufour@havenstudios.com>
2026-03-11 16:25:36 -0400

    Start doing project settings to get project path

---
+++ core/main/project_settings.cpp
@@ -0,0 +1,22 @@
+﻿#include "project_settings.h"
+
+namespace feather {
+
+std::unique_ptr<ProjectSettings> ProjectSettings::_instance = nullptr;
+
+ProjectSettings::ProjectSettings() : _project_path(FileSystem::current_path()) {}
+
+ProjectSettings* ProjectSettings::get() {
+	if (_instance)
+		_instance = std::make_unique<ProjectSettings>();
+
+	return _instance.get();
+}
+
+void ProjectSettings::_bind_members() {
+	ClassDB::bind_property(&ProjectSettings::_project_path, "project_path", VariantType::STRING);
+}
+
+void ProjectSettings::set_project_path(Path path) { _project_path = path; }
+
+} //namespace feather
---
+++ core/main/project_settings.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include <framework/path.h>
+#include <framework/reflected.h>
+#include <framework/reflection_macros.h>
+
+namespace feather {
+
+class ProjectSettings final : public Reflected {
+	FCLASS(ProjectSettings, Reflected);
+
+	friend class Engine;
+
+	Path _project_path;
+
+	static std::unique_ptr<ProjectSettings> _instance;
+	friend std::unique_ptr<ProjectSettings> std::make_unique<ProjectSettings>();
+
+protected:
+	ProjectSettings();
+	static void _bind_members();
+
+	void set_project_path(Path path);
+
+public:
+	static ProjectSettings* get();
+};
+
+} //namespace feather
