0c3680cee3f097f38e5f8f6349ce71aa170fe0b3
md <martin.dufour@havenstudios.com>
2026-03-19 10:29:16 -0400

    Add mesh format loader

---
+++ core/resources/mesh_format_loader.cpp
@@ -0,0 +1,7 @@
+﻿#include "mesh_format_loader.h"
+
+namespace feather {
+
+void MeshFormatLoader::_bind_members() {}
+
+} // namespace feather
---
+++ core/resources/mesh_format_loader.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "resource_format_loader.h"
+
+namespace feather {
+
+class MeshFormatLoader : public ResourceFormatLoader {
+	FCLASS(MeshFormatLoader, ResourceFormatLoader);
+
+protected:
+	static void _bind_members();
+};
+
+} //namespace feather
