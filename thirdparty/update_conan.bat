0f18a1866dfb02f777a56395cdc7ba7404e8a3fb
Martin Dufour <sonic99@live.ca>
2026-04-03 16:28:18 -0400

    Removed conanfiles which are deprecated.

--- thirdparty/update_conan.bat
+++
@@ -1,3 +0,0 @@
-conan install . --build=missing --output-folder=conan
-conan install . --build=missing -s build_type=Debug --output-folder=conan
-conan install . --build=missing -s build_type=RelWithDebInfo --output-folder=conan
