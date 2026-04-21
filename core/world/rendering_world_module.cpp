0d5f78fe0af0bcdde4aca4c539378b4d44766c60
md <martin.dufour@havenstudios.com>
2026-04-21 16:43:52 -0400

    Start adding first systems.

--- core/world/rendering_world_module.cpp
+++ core/world/rendering_world_module.cpp
@@ -2,6 +2,7 @@
 
 #include "components/light.h"
 #include "main/world_sim.h"
+#include "rendering/rendering_server.h"
 
 namespace feather {
 
@@ -20,6 +21,25 @@ RenderingWorldFeature::RenderingWorldFeature(World world) {
 	world.component<MeshInstance>();
 	world.component<MaterialInstance>();
 	world.component<Light>();
+
+	// todo add iterative way to create buffers
+	// world.observer<MeshInstance>()
+	// 		.with<const Transform, MaterialInstance>()
+	// 		.optional()
+	// 		.event(flecs::OnSet)
+	// 		.each([](Entity e, const Transform& t, MeshInstance& mi, MaterialInstance* mat) {
+	// 			RenderingServer::get()->;
+	// 		});
+
+	world.system("Create Render Scene").kind(flecs::PreStore).run([](const flecs::iter& it) {
+		world.set<RenderScene>();
+	});
+
+	world.system<Transform, MeshInstance, MaterialInstance*>()
+			.kind(flecs::PreStore)
+			.each([](Entity e, Transform transform, MeshInstance& mesh, MaterialInstance* instance) {
+				auto& renderScene = world.get<RenderScene>();
+			});
 }
 
 } //namespace feather
